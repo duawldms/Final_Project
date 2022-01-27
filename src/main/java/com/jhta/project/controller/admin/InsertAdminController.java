38cd78b421bc59945bf5dc4de99bf93802ddf52a
정인숙 <@LAPTOP-LSF5PPAO>
2022-01-27 09:07:16 +0900

    admin login

---
+++ src/main/java/com/jhta/project/controller/admin/InsertAdminController.java
@@ -0,0 +1,39 @@
+package com.jhta.project.controller.admin;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.jhta.project.service.admin.AdminService;
+import com.jhta.project.vo.admin.AdminVo;
+
+@Controller
+public class InsertAdminController {
+	@Autowired AdminService service;
+	
+	@GetMapping("/insertadmin")
+	public String AdminInsertForm(Model model) {
+		model.addAttribute("main","/WEB-INF/views/admin/insert.jsp");
+		return "layout";
+	}
+	@PostMapping("/insertadmin")
+	public String UserInsert(AdminVo vo,Model model,HttpServletRequest req,HttpServletResponse resp) {
+		try {
+			model.addAttribute("result","회원가입 완료!");
+			model.addAttribute("main","/WEB-INF/views/admin/result.jsp");
+		}catch(Exception e){
+			e.printStackTrace();
+			model.addAttribute("result","false");
+			model.addAttribute("main","/WEB-INF/views/admin/result.jsp");
+		}
+		return "layout";
+	}
+
+}
+
+
