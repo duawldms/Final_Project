b7212021c718ca606d12ecde6975186ac85a5396
yhw92 <yhw92@220.121.11.140>
2022-02-04 17:20:23 +0900

    Merge branch 'master' of https://github.com/HyunwooY/Final_Project.git

--- src/main/java/com/jhta/project/controller/user/GoCartController.java
+++ src/main/java/com/jhta/project/controller/user/GoCartController.java
@@ -23,9 +23,9 @@ public class GoCartController {
 	@Autowired private SearchService service;
 	
 	@GetMapping(value="/user/gocart",produces= {MediaType.APPLICATION_JSON_VALUE})
-	public HashMap<String, Object> gocart(@RequestParam(value="checkbox[]") List<String> checkbox,
-			@RequestParam(value="optionscnt[]") List<String> optionscnt,int foodnum,
-			@RequestParam(value="necoptions[]") List<String> nec,Model model,Principal principal) {
+	public HashMap<String, Object> gocart(@RequestParam(value="checkbox[]",defaultValue = "") List<String> checkbox,
+			@RequestParam(value="optionscnt[]",defaultValue = "") List<String> optionscnt,int foodnum,
+			@RequestParam(value="necoptions[]",defaultValue = "") List<String> nec,Model model,Principal principal) {
 		HashMap<String, Object> map=new HashMap<String, Object>();
 		try {
 			String ui_id=principal.getName();
